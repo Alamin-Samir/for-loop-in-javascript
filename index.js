7f931ee86461cd6a49726789400478e141ead87b
samir <sa.samir213002019@gmail.com>
2025-11-05 10:33:07 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+let fruits = ["aplle","orange","banana","mango"]
+
+
+let length=fruits.length
+
+console.log(length)
+
+
+console.log(fruits[1])
+
+
+
+for(let i=0; i<4; i++){
+
+//run koarr jonne terminal a gie node index.js 
+//node die spce 
+
+
+console.log(fruits[i])
+
+
+
+
+
+
+
+}
+
+
+
+
+for(let i=0; i<=10; i++){
+
+
+
+if(i%2==0){
+
+    console.log(i+"even")
+}
+
+else{
+
+    console.log(i+"odd")
+
+}
+
+
+console.log(i);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
